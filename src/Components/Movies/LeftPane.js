1cbdd17fb0a2d90bdc7adb491da30f3df825213d
uzairtq <uzairtq@gmail.com>
2020-04-21 01:21:43 -0400

    Left and Right Pane added

---
+++ src/Components/Movies/LeftPane.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Paper } from "@material-ui/core";
+
+export default ({ styles }) => (
+  <Paper style={styles.Pane_Paper}>Left Pane</Paper>
+);
--- src/Components/Movies/index.js
+++ src/Components/Movies/index.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import LeftPane from "./LeftPane";
+import RightPane from "./RightPane";
+
+const styles = {
+  Pane_Paper: { padding: 20, marginTop: 10, marginBotton: 10 }
+};
 
 export default props => (
-  <div>
-    <h2>Hello from Movies!!</h2>
-  </div>
+  <Grid container>
+    <Grid item sm>
+      <LeftPane styles={styles} />
+    </Grid>
+    <Grid item sm>
+      <RightPane styles={styles} />
+    </Grid>
+  </Grid>
 );
